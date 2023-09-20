0aff0674cfe6890d189171c3d2bab1cfd985ef23
AmanSingh071 <aman18031607.com>
2023-09-20 17:37:38 +0530

    small

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/AmanSingh071/portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
